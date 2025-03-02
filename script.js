b13aa0c8c2762215515d17eae92f3361666f684a
salcedoma <salcedoma.github@gmail.com>
2025-03-02 00:58:54 -0300

    Add comments for isntructions

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ const Game = (function () {
 
     // CREATE NEW INSTANCE OF GAME(ASK FOR PLAYER INFO, RENDER GAMEBOARD, WAIT FOR PLAYER MOVE)
     function newGame() {
-        let player1 = player(prompt("Player 1?"), "X");
-        let player2 = player(prompt("Player 2?"), "O");
+        let player1 = player(prompt("Player 1 name?"), "X");
+        let player2 = player(prompt("Player 2 name?"), "O");
         let currentGame = gameboard();
         currentGame.render(player1, player2)
         return function move(player, posX, posY) { //CHECK WHICH PLAYER, PLACE "PIECE"
@@ -42,3 +42,7 @@ const Game = (function () {
 
 
 const gameOne = Game.newGame();
+
+// in order to make the first move I would write:
+// gameOne(1, 1, 3) -> (player "1", x-coordinate "1", y-coordinate "3");
+// gameOne(2, 3, 2) -> (player "2", x-coordinate "3", y-coordinate "2");
